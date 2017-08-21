4e1c9056297802921e4015744acbb50ab4396ad3
efraimmgon <efraimmaugusto@gmail.com>
2017-08-21 09:48:18 -0400

    add admin to user migration

--- resources/migrations/20161105193436-add-users-table.up.sql
+++ resources/migrations/20161105193436-add-users-table.up.sql
@@ -7,3 +7,7 @@ CREATE TABLE users
  last_login TIMESTAMP,
  is_active BOOLEAN,
  pass VARCHAR(300) NOT NULL);
+ --;;
+ -- create a system account for which the stock entries will be created
+ INSERT INTO users (id, email, admin, is_active, pass)
+ VALUES ('admin', 'admin@admin.com', TRUE, TRUE, 'bcrypt+sha512$86186fc28f83b3e3db78bcf8350a3a57$12$8f215420e68fd7922561167b07354f05d8db6d49e212689e');
