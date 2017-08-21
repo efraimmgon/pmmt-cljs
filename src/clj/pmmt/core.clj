96e684ec27f9c63c6924e2ead70e9c5e645e2685
efraimmgon <efraimmaugusto@gmail.com>
2017-08-21 09:38:01 -0400

    run migrations on app start

--- src/clj/pmmt/core.clj
+++ src/clj/pmmt/core.clj
@@ -44,6 +44,7 @@
                         mount/start-with-args
                         :started)]
     (log/info component "started"))
+  (migrations/migrate ["migrate"] (select-keys env [:database-url]))
   (.addShutdownHook (Runtime/getRuntime) (Thread. stop-app)))
 
 (defn -main [& args]
